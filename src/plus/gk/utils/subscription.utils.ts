cc41f43408b45db38847865d1d28cd63f96a60e5
Xinglin Qiang <xinglinqiang@gmail.com>
2025-11-12 10:04:25 +0800

    魔改

--- src/plus/gk/utils/subscription.utils.ts
+++ src/plus/gk/utils/subscription.utils.ts
@@ -281,7 +281,7 @@ export function getCommunitySubscription(subscription?: Subscription): Subscript
 		...subscription,
 		plan: {
 			actual: getSubscriptionPlan(
-				'community',
+				'enterprise',
 				false,
 				0,
 				undefined,
@@ -290,7 +290,7 @@ export function getCommunitySubscription(subscription?: Subscription): Subscript
 					: undefined,
 			),
 			effective: getSubscriptionPlan(
-				'community',
+				'enterprise',
 				false,
 				0,
 				undefined,
@@ -301,6 +301,6 @@ export function getCommunitySubscription(subscription?: Subscription): Subscript
 		},
 		account: undefined,
 		activeOrganization: undefined,
-		state: SubscriptionState.Community,
+		state: SubscriptionState.Paid,
 	};
 }
